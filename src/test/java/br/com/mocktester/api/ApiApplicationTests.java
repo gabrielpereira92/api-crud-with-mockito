8079e9d494ba86c8da03e490d8cc84b1e028d6b3
Gabriel Ramon Ferreira Pereira <gabriel_rfp@outlook.com>
2023-01-03 19:08:20 -0300

    projeto iniciado

---
+++ src/test/java/br/com/mocktester/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mocktester.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
