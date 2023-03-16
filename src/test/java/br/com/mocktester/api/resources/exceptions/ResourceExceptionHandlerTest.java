bae6424caf1f11a7c970086c1ec045ae140a2dd6
Gabriel Ramon Ferreira Pereira <gabriel_rfp@outlook.com>
2023-03-16 17:12:46 -0300

    teste classe de excecoes metodo objectNotFound

---
+++ src/test/java/br/com/mocktester/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,45 @@
+package br.com.mocktester.api.resources.exceptions;
+
+import br.com.mocktester.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ResourceExceptionHandlerTest {
+
+    @InjectMocks
+    private ResourceExceptionHandler resourceExceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = resourceExceptionHandler
+                .objectNotFound(new ObjectNotFoundException("Objeto não encontrado"), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Objeto não encontrado", response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+
+    }
+
+    @Test
+    void validationEmail() {
+    }
+}
